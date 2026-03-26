080f16008c138887f8b8335750a514bd118ee827
Santiago Fernández Sánchez <santiagofernandezsanchezfersa@gmail.com>
2026-03-26 12:52:43 +0100

    Refactor App class to use Utils for input handling and add export functionality for dragon list

--- proyecto-extra/dragones/src/App.java
+++ proyecto-extra/dragones/src/App.java
@@ -5,23 +5,12 @@ import java.util.Scanner;
 public class App {
     private static Scanner scanner = new Scanner(System.in);
 
-    private static int askInt(String prompt) {
-        System.out.print(prompt);
-
-        try {
-            return Integer.parseInt(scanner.nextLine());
-        } catch (NumberFormatException e) {
-            System.out.println("Entrada no válida. Por favor, introduce un número entero.");
-        }
-
-        return -1;
-    }
     public static void main(String[] args) {
         List<Dragon> dragons = new ArrayList<>();
         
         int option = 0;
         
-        while (option != 7) {
+        while (option != 8) {
             System.out.println("Menú:");
             System.out.println("1. Añadir dragón");
             System.out.println("2. Mostrar listado de dragones");
@@ -29,45 +18,43 @@ public class App {
             System.out.println("4. Buscar dragón por nombre");
             System.out.println("5. Modificar dragón");
             System.out.println("6. Eliminar dragón");
-            System.out.println("7. Salir");
+            System.out.println("7. Exportar lista de dragones");
+            System.out.println("8. Salir");
             
-            option = askInt("Introduce una opción: ");
+            option = Utils.askInt("Introduce una opción: ", scanner);
 
             if (option == 1) {
-                int id = askInt("Introduce el ID del dragón: ");
-                if (id == -1) {
+                int id = Utils.askInt("Introduce el ID del dragón: ", scanner);
+                if (Utils.idExists(dragons, id)) {
+                    System.out.println("Ya existe un dragón con ese ID.");
                     continue;
                 }
 
-                System.out.println("Introduce el nombre del dragón:");
+                System.out.print("Introduce el nombre del dragón: ");
                 String name = scanner.nextLine();
-                System.out.println("Introduce el tipo del dragón:");
-                String typeString = scanner.nextLine();
+                if (Utils.nameExists(dragons, name)) {
+                    System.out.println("Ya existe un dragón con ese nombre.");
+                    continue;
+                }
                 DragonType type = null;
-
-                while (typeString == null) {
+                while (type == null) {
+                    System.out.print("Introduce el tipo del dragón (" + Utils.arrayToString(DragonType.values()) + "): ");
+                    String typeString = scanner.nextLine();
                     try {
-                        type = DragonType.valueOf(scanner.nextLine().toUpperCase());
+                        type = DragonType.valueOf(typeString.toUpperCase());
                     } catch (Exception e) {
-                        System.out.println("Tipo no válido. Por favor, introduce un tipo válido (FUEGO, AGUA, TIERRA, VIENTO, LUZ, OSCURIDAD):");
-                        continue;
+                        System.out.println("Tipo no válido.");
                     }
                 }
 
-                int level = askInt("Introduce el nivel del dragón: ");
-                if (level == -1) {
+                int level = Utils.askInt("Introduce el nivel del dragón: ", scanner);
+                if (level < 0) {
+                    System.out.println("Nivel no válido. El nivel debe ser un número entero positivo.");
                     continue;
                 }
 
-                int attack = askInt("Introduce el ataque del dragón: ");
-                if (attack == -1) {
-                    continue;
-                }
-
-                int defense = askInt("Introduce la defensa del dragón: ");
-                if (defense == -1) {
-                    continue;
-                }
+                int attack = Utils.askInt("Introduce el ataque del dragón: ", scanner);
+                int defense = Utils.askInt("Introduce la defensa del dragón: ", scanner);
 
                 dragons.add(new Dragon(id, name, type, level, attack, defense));
             } else if (option == 2) {
@@ -75,7 +62,7 @@ public class App {
                     System.out.println("ID: " + dragon.id + ", Nombre: " + dragon.name + ", Tipo: " + dragon.type + ", Nivel: " + dragon.level + ", Ataque: " + dragon.attack + ", Defensa: " + dragon.defense);
                 }
             } else if (option == 3) {
-                int id = askInt("Introduce el ID del dragón a buscar: ");
+                int id = Utils.askInt("Introduce el ID del dragón a buscar: ", scanner);
                 Dragon foundDragon = null;
 
                 for (Dragon dragon : dragons) {
@@ -91,7 +78,7 @@ public class App {
                     System.out.println("Dragón no encontrado.");
                 }
             } else if (option == 4) {
-                System.out.println("Introduce el nombre del dragón a buscar: ");
+                System.out.print("Introduce el nombre del dragón a buscar: ");
                 String name = scanner.nextLine();
                 Dragon foundDragon = null;
 
@@ -109,7 +96,7 @@ public class App {
                     System.out.println("Dragón no encontrado.");
                 }
             } else if (option == 5) {
-                int id = askInt("Introduce el ID del dragón a modificar: ");
+                int id = Utils.askInt("Introduce el ID del dragón a modificar: ", scanner);
                 Dragon foundDragon = null;
 
                 for (Dragon dragon : dragons) {
@@ -120,41 +107,33 @@ public class App {
                 }
 
                 if (foundDragon != null) {
-                    System.out.println("Introduce el nuevo nombre del dragón:");
+                    System.out.print("Introduce el nuevo nombre del dragón: ");
                     foundDragon.name = scanner.nextLine();
-                    System.out.println("Introduce el nuevo tipo del dragón:");
-                    String typeString = scanner.nextLine();
                     DragonType type = null;
-
-                    while (typeString == null) {
+                    while (type == null) {
+                        System.out.print("Introduce el nuevo tipo del dragón (" + Utils.arrayToString(DragonType.values()) + "): ");
+                        String typeString = scanner.nextLine();
                         try {
-                            type = DragonType.valueOf(scanner.nextLine().toUpperCase());
+                            type = DragonType.valueOf(typeString.toUpperCase());
                         } catch (Exception e) {
-                            System.out.println("Tipo no válido. Por favor, introduce un tipo válido (FUEGO, AGUA, TIERRA, VIENTO, LUZ, OSCURIDAD):");
-                            continue;
+                            System.out.println("Tipo no válido.");
                         }
                     }
                     foundDragon.type = type;
 
-                    int level = askInt("Introduce el nuevo nivel del dragón: ");
-                    if (level != -1) {
-                        foundDragon.level = level;
-                    }
+                    int level = Utils.askInt("Introduce el nuevo nivel del dragón: ", scanner);
+                    foundDragon.level = level;
 
-                    int attack = askInt("Introduce el nuevo ataque del dragón: ");
-                    if (attack != -1) {
-                        foundDragon.attack = attack;
-                    }
+                    int attack = Utils.askInt("Introduce el nuevo ataque del dragón: ", scanner);
+                    foundDragon.attack = attack;
 
-                    int defense = askInt("Introduce la nueva defensa del dragón: ");
-                    if (defense != -1) {
-                        foundDragon.defense = defense;
-                    }
+                    int defense = Utils.askInt("Introduce la nueva defensa del dragón: ", scanner);
+                    foundDragon.defense = defense;
                 } else {
                     System.out.println("Dragón no encontrado.");
                 }
             } else if (option == 6) {
-                int id = askInt("Introduce el ID del dragón a eliminar: ");
+                int id = Utils.askInt("Introduce el ID del dragón a eliminar: ", scanner);
                 Dragon foundDragon = null;
 
                 for (Dragon dragon : dragons) {
@@ -171,9 +150,28 @@ public class App {
                     System.out.println("Dragón no encontrado.");
                 }
             } else if (option == 7) {
+                System.out.println("Formatos de exportación disponibles:");
+                System.out.println("1. CSV");
+                System.out.println("2. JSON");
+                System.out.println("3. XLSX");
+
+                int exportOption = Utils.askInt("Introduce una opción de exportación: ", scanner);
+                if (exportOption == 1) {
+                    System.out.print("Exportando a CSV...\nIntroduce el nombre del archivo (con extensión .csv): ");
+                    Utils.exportToCSV(dragons, scanner.nextLine());
+                } else if (exportOption == 2) {
+                    System.out.print("Exportando a JSON...\nIntroduce el nombre del archivo (con extensión .json): ");
+                    Utils.exportToJSON(dragons, scanner.nextLine());
+                } else if (exportOption == 3) {
+                    System.out.print("Exportando a XLSX...\nIntroduce el nombre del archivo (con extensión .xlsx): ");
+                    Utils.exportToXLSX(dragons, scanner.nextLine());
+                } else {
+                    System.out.println("Opción de exportación no válida.");
+                }
+            } else if (option == 8) {
                 System.out.println("Saliendo del programa...");
             } else {
-                System.out.println("Opción no válida. Por favor, introduce una opción del 1 al 7.");
+                System.out.println("Opción no válida. Por favor, introduce una opción del 1 al 8.");
             }
         }
 
